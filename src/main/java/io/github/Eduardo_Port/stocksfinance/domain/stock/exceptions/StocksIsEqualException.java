ea54bbdf3e5f6c9a0df07f35379d95124314c58a
Eduardo <edu.friv10@gmail.com>
2025-03-20 21:07:11 -0300

    classe StockIsEqualException arrumada

--- src/main/java/io/github/Eduardo_Port/stocksfinance/domain/stock/exceptions/StocksIsEqualException.java
+++ src/main/java/io/github/Eduardo_Port/stocksfinance/domain/stock/exceptions/StocksIsEqualException.java
@@ -1,8 +1,6 @@
 package io.github.Eduardo_Port.stocksfinance.domain.stock.exceptions;
 
-<<<<<<< HEAD
+
 public class StocksIsEqualException extends RuntimeException{
-=======
-public class StocksIsEqualException extends RuntimeException {
->>>>>>> 6adc5d8 (melhorando o tratamento de exceções)
+
 }
